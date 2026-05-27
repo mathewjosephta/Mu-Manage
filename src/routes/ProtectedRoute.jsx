8ca8143c1667cf373f916a590a72d37444520eeb
mathewjosephta <mathewjoseph3831v@gmail.com>
2026-05-27 07:59:16 +0530

    added ProtectedRoute.jsx

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -4,18 +4,44 @@ from "react-router-dom";
 function ProtectedRoute({
 
   userRole,
-  allowedRoles,
+  allowedRoles = [],
   children
 
 }) {
 
-  if (
-    !allowedRoles.includes(userRole)
-  ) {
+  // NO ROLE
 
-    return <Navigate to="/" />;
+  if (!userRole) {
+
+    return (
+      <Navigate
+        to="/login"
+        replace
+      />
+    );
 
   }
+
+  // ACCESS DENIED
+
+  if (
+
+    !allowedRoles.includes(
+      userRole
+    )
+
+  ) {
+
+    return (
+      <Navigate
+        to="/"
+        replace
+      />
+    );
+
+  }
+
+  // ALLOWED
 
   return children;
 
